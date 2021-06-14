c560c7734e77ffbc536461c9ec0c0f11574f92e0
KiichiHamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-06-14 11:55:03 +0900

    チャット開始ボタンにCSSを適用した,描画開始時にチャット参加リクエストを発行するように変更した

--- client/conponents/StartMatchButton.tsx
+++ client/conponents/StartMatchButton.tsx
@@ -1,26 +1,37 @@
 import React, {useState, useContext, useEffect} from 'react';
 import {SocketContext} from 'context/socket';
-import {userInfoContext} from 'context/userInfo';
+import {userInfoContext} from 'context/userInfo'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  width: 150px;
+  border-radius: 5px;
+  color: #fff;
+  padding: 8px 16px;
+  font-size: 16px;
+  border: 1px solid lightgray;
+  background-color: silver;
+`
 
 const StartMatchButton = () => {
     const socket = useContext(SocketContext);
     const userInfo = useContext(userInfoContext)
-    const [isJoin,setIsJoin] = useState(false)
+    const [isJoin,setIsJoin] = useState(true)
 
     useEffect(() => {
         console.log(`${isJoin}`)
         if(isJoin){
             console.log(userInfo)
-            socket.emit("JOIN_REQUEST");
+            socket.emit("JOIN_REQUEST",userInfo);
         }else{
             socket.emit("LEAVE_REQUEST");
         }
     }, [isJoin]);
 
     return(
-        <button onClick={()=>setIsJoin(!isJoin)}>
+        <Button onClick={()=>setIsJoin(!isJoin)}>
             { isJoin ? 'チャット終了' : '新規チャット' }
-        </button>
+        </Button>
     )
 }
 
