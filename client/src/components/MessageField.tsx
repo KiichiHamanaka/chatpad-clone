5c011cc9416634b3732813a1bbad5967273f8417
KiichiHamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-06-16 15:47:18 +0900

    新たな型定義に沿ったpropsに変更

--- client/src/components/MessageField.tsx
+++ client/src/components/MessageField.tsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
-import { MessageTypes, UserType } from "types";
 import { SocketContext } from "../context/socket";
 import Message from "./Message";
 import styled from "styled-components";
+import { MessageTypes, UserType } from "../../../types";
+import { SystemMessages } from "../../../SystemMessages";
 
 const MessageBox = styled.div`
   overflow: scroll;
@@ -33,8 +34,8 @@ const MessageField = () => {
   const socket = useContext(SocketContext);
   const [messages, setMessages] = useState<Array<MessageTypes>>([
     {
-      author: "SYSTEM",
-      body: "どもー。ChatPadシステムです(・Д・)ノチャットが始まる前に少しだけ利用規約のお話をさせてね！\n・楽しいチャットにするために相手を不快にする発言はしないでね\n・恋人探しや、会うことだけを目的にした利用はしないようにね\n個人情報は自分で守ってね\nチャット本文やスクリーンショットを第三者に公開しないでね\n違法行為や犯罪行為、営利行為につながる発言は禁止だよ\n\nみんなが楽しく使えるチャットサービスにするためにChatPadとの約束だよ！ (・Д・)b",
+      callBy: "SYSTEM",
+      body: SystemMessages.Connect,
     },
   ]);
   const [enemy, setEnemy] = useState<UserType>();
@@ -49,8 +50,8 @@ const MessageField = () => {
   });
   socket.on("MATCH_START", (data) => {
     const message: MessageTypes = {
-      author: "SYSTEM",
-      body: "ChatPadシステムです (・Д・)\nチャット相手が見つかったので\nチャットを始めるよー！\n",
+      callBy: "SYSTEM",
+      body: SystemMessages.StartMatch,
     };
     console.log(`match!`);
     console.log(data);
@@ -63,7 +64,12 @@ const MessageField = () => {
     return (
       <MessageBox>
         {messages.map((message, index) => (
-          <Message key={index} author={message.author} body={message.body} />
+          <Message
+            key={index}
+            author={message.author}
+            callBy={message.callBy}
+            body={message.body}
+          />
         ))}
       </MessageBox>
     );
