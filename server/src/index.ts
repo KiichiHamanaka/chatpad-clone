116e89e68f163367a18377d682e75da7c2bc5e28
KiichiHamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-06-16 15:44:14 +0900

    コメントの削除

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,64 +1,50 @@
-import express from 'express'
-import { Server,Socket } from 'socket.io';
-import eventHandle from './eventHandler';
+import express from "express";
+import { Server, Socket } from "socket.io";
+import eventHandle from "./eventHandler";
 
-const app: express.Express = express()
+const app: express.Express = express();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-})
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
 
-const server = app.listen(3001,()=>{ console.log('Example app listening on port 3001!') })
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const server = app.listen(3001, () => {
+  console.log("Example app listening on port 3001!");
+});
 
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ["GET", "POST"]
-    },
-})
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-io.on('connection', async (socket: Socket) => {
-    let activeUsers = await io.fetchSockets()
-    const handler = new eventHandle(io,socket)
-    socket.emit('ACTIVE_USERS',activeUsers.length)
-    socket.on("JOIN_REQUEST",async () => {
-        await handler.joinRequest()
-    })
-    socket.on("LEAVE_REQUEST",async () => {
-        await handler.leaveRequest()
-    })
-    socket.on("MESSAGE",async (data) => {
-        await handler.sendMessage(data)
-    })
-    socket.on("disconnect",async () => {
-        await handler.disconnect()
-    })
-})
-
-
-// io.on('connection', async (socket: Socket) => {
-//     let activeUsers = await io.fetchSockets()
-//     // let eventHandle = new EventHandler()
-//     socket.emit('ACTIVE_USERS',activeUsers.length)
-//     socket.on("JOIN_REQUEST",async () => {
-//         await eventHandler(io,socket,"JOIN_REQUEST")
-//     })
-//     socket.on("LEAVE_REQUEST",async () => {
-//         await eventHandler(io,socket,"LEAVE_REQUEST")
-//     })
-//     socket.on("MESSAGE",async (data) => {
-//         await eventHandler(io,socket,"MESSAGE",data)
-//     })
-//     socket.on("disconnect",async () => {
-//         await eventHandler(io,socket,"disconnect")
-//     })
-// })
+io.on("connection", async (socket: Socket) => {
+  let activeUsers = await io.fetchSockets();
+  const handler = new eventHandle(io, socket);
+  socket.emit("ACTIVE_USERS", activeUsers.length);
+  socket.on("JOIN_REQUEST", async () => {
+    await handler.joinRequest();
+  });
+  socket.on("LEAVE_REQUEST", async () => {
+    await handler.leaveRequest();
+  });
+  socket.on("MESSAGE", async (data) => {
+    await handler.sendMessage(data);
+  });
+  socket.on("disconnect", async () => {
+    await handler.disconnect();
+  });
+});
