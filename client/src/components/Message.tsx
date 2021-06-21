39b66240b798626a0b414a2ab7c386ccd70f19d3
KiichiHamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-06-22 01:44:04 +0900

    メッセージが300px以上のとき折り返すように設定

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -5,27 +5,32 @@ import { socket } from "../context/socket";
 
 const System = styled.div`
   padding: 8px 16px;
-  margin: 5px 5px;
+  margin: 5px auto;
   background-color: #ffe2c6;
   border: 0.5px solid white;
   border-radius: 5px;
 `;
+
 const Me = styled.div`
+  max-width: 300px;
   width: fit-content;
   margin: 5px 0 5px auto;
   padding: 8px 16px;
-  background-color: lightgreen;
   border: 0.5px solid white;
   border-radius: 5px;
+  background-color: lightgreen;
+  word-break: break-all;
 `;
+
 const Enemy = styled.div`
+  max-width: 300px;
   width: fit-content;
   margin: 5px auto 5px 0;
-  text-align: left;
   padding: 8px 16px;
   border: 0.5px solid white;
   border-radius: 5px;
   background-color: lightgrey;
+  word-break: break-all;
 `;
 
 const Message = (props: MessageTypes) => {
