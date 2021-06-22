5b6cfdcc53cc4ea1cd11cb8f60ec8cc8c871609e
KiichiHamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-06-22 18:30:23 +0900

    margin,widthを他コンポーネントと合わせた

--- client/src/components/ProfileIcon.tsx
+++ client/src/components/ProfileIcon.tsx
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
 import Image from "next/image";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  border-radius: 5px;
+  border: solid 1px lightgray;
+  padding: 2px;
+  width: auto;
+  height: auto;
+`;
 
 const ProfileIcon = () => {
   const [imagePath, setImagePath] = useState("/zikannai.png");
 
   return (
-    <Image src={imagePath} alt="picture of profile" width={156} height={156} />
+    <Wrap>
+      <Image
+        src={imagePath}
+        alt="picture of profile"
+        width={140}
+        height={140}
+      />
+    </Wrap>
   );
 };
 
