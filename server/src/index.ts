e1e9b03d4bbdfca708def210ca956d04acfb9f4e
KiichiHamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-06-23 22:01:23 +0900

    corsの設定

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,7 +26,7 @@ const server = app.listen(process.env.PORT || 3001, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatpad-clone.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
